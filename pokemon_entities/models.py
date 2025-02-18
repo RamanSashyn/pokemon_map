55da827e27f184f0fcc8e11c8c11b20b689cace3
Raman <149716575+RamanSashyn@users.noreply.github.com>
2025-02-18 15:05:19 +0100

    Исправление типа поля

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -21,17 +21,22 @@ class Pokemon(models.Model):
 
 
 class PokemonEntity(models.Model):
-    pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE, verbose_name='Покемон')
+    pokemon = models.ForeignKey(
+        Pokemon,
+        on_delete=models.CASCADE,
+        related_name='entities',
+        verbose_name='Покемон'
+    )
     lat = models.FloatField(verbose_name='Широта')
     lon = models.FloatField(verbose_name='Долгота')
     appeared_at = models.DateTimeField(verbose_name="Время появления", null=True, blank=True)
     disappeared_at = models.DateTimeField(verbose_name="Время исчезновения", null=True, blank=True)
 
-    level = models.IntegerField(null=True, blank=True, verbose_name='Уровень')
-    health = models.IntegerField(null=True, blank=True, verbose_name='Здоровье')
-    strength = models.IntegerField(null=True, blank=True, verbose_name='Сила')
-    defense = models.IntegerField(null=True, blank=True, verbose_name='Защита')
-    stamina = models.IntegerField(null=True, blank=True, verbose_name='Выносливость')
+    level = models.PositiveIntegerField(null=True, blank=True, verbose_name='Уровень')
+    health = models.PositiveIntegerField(null=True, blank=True, verbose_name='Здоровье')
+    strength = models.PositiveIntegerField(null=True, blank=True, verbose_name='Сила')
+    defense = models.PositiveIntegerField(null=True, blank=True, verbose_name='Защита')
+    stamina = models.PositiveIntegerField(null=True, blank=True, verbose_name='Выносливость')
 
 def __str__(self):
         return f'{self.pokemon.title} {self.lat} {self.lon}'
