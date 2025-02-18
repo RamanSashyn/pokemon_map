a69e1e815310faa547b08d97752e8cb4e89b0121
Raman <149716575+RamanSashyn@users.noreply.github.com>
2025-02-18 14:54:26 +0100

    Файлы миграции

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -3,8 +3,8 @@ from django.db import models  # noqa F401
 
 class Pokemon(models.Model):
     title = models.CharField(max_length=200, blank=False, verbose_name='Название покемона')
-    title_en = models.CharField(max_length=200, blank=True, null=True, verbose_name='Название покемона на английском')
-    title_jp = models.CharField(max_length=200, blank=True, null=True, verbose_name='Название покемона на японском')
+    title_en = models.CharField(max_length=200, blank=True, default='', verbose_name='Название покемона на английском')
+    title_jp = models.CharField(max_length=200, blank=True, default='', verbose_name='Название покемона на японском')
     description = models.TextField(blank=True, verbose_name='Описание покемона')
     photo = models.ImageField(upload_to='pokemons', blank=True, null=True, verbose_name='Фото покемона')
     previous_evolution = models.ForeignKey(
@@ -24,8 +24,8 @@ class PokemonEntity(models.Model):
     pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE, verbose_name='Покемон')
     lat = models.FloatField(verbose_name='Широта')
     lon = models.FloatField(verbose_name='Долгота')
-    appeared_at = models.DateTimeField("Время появления", default=None)
-    disappeared_at = models.DateTimeField("Время исчезновения", default=None)
+    appeared_at = models.DateTimeField(verbose_name="Время появления", null=True, blank=True)
+    disappeared_at = models.DateTimeField(verbose_name="Время исчезновения", null=True, blank=True)
 
     level = models.IntegerField(null=True, blank=True, verbose_name='Уровень')
     health = models.IntegerField(null=True, blank=True, verbose_name='Здоровье')
